799fe8f1cc1fad2c30a09bd0afbd203067ed3b2b
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-09-01 11:35:02 +0300

    basic prop types

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Person from './components/Person';
+import PersonList from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Alwyn',
+    last: 'Madiba'
+  }
+
+  const nameList = [
+    {
+      first: 'Alwyn',
+      last: 'Madiba'
+    },
+    {
+      first: 'Nelson',
+      last: 'Osho'
+    },
+    {
+      first: 'Alfred',
+      last: 'Sadhguru'
+    }
+  ]
+
   return (
     <div className="App">
-      <Greet name="Alwyn"/>
+      <Greet name="Alwyn" messageCount={12} isLoggedIn={false}/>
+      <Person name={personName}/>
+      <PersonList names={nameList}/>
     </div>
   );
 }
